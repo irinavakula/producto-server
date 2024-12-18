0e1f8e9543b4cd00676dee5b212e8fe200b6c543
Iryna Bohomiakova <irinkavakula@gmail.com>
2024-12-18 13:58:48 +0100

    add style en html

---
+++ front/index.js
@@ -0,0 +1,27 @@
+const products = [
+    {
+        id: 1, 
+        name: "About product",
+        price: 100,
+        image: "product1.jpg"
+    },
+    {
+        id: 2, 
+        name: "About product",
+        price: 200,
+        image: "product2.jpg"   
+    },
+    {
+        id: 3, 
+        name: "About product",
+        price: 300,
+        image: "product3.jpg"
+    },
+    {
+        id: 4, 
+        name: "About product",
+        price: 400,
+        image: "product4.jpg"
+    }
+
+];
